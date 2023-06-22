71e30d85f13306a5767b6c64ed0cbbe230ae146b
Javier Albert <130755203+XaviAlbert@users.noreply.github.com>
2023-06-22 17:39:25 +0200

    Add files via upload

---
+++ src/evaluation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
+import joblib
+
+test_data = pd.read_csv('data/test/test.csv')
+
+# 1. Regresión lineal
+
+numeric_columns = test_data.select_dtypes(include='number')
+
+X_test = numeric_columns.drop('NOTA', axis=1) 
+y_test = numeric_columns['NOTA']
+
+regression_model = joblib.load('models/regresion_lineal.pkl')
+
+y_pred = regression_model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+
+print("Regresión lineal (MSE):", mse)
---
+++ src/model.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.cluster import KMeans
+import joblib
+
+df = pd.read_csv('data/processed/data.csv')
+
+X = df.drop('NOTA', axis=1)
+y = df['NOTA']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+train_data = pd.concat([X_train, y_train], axis=1)
+test_data = pd.concat([X_test, y_test], axis=1)
+
+train_data.to_csv('data/train/train.csv', index=False)
+test_data.to_csv('data/test/test.csv', index=False)
+
+train_data = pd.read_csv('data/train/train.csv')
+
+# 1. Regresión lineal
+
+numeric_columns = train_data.select_dtypes(include='number')
+
+X = numeric_columns.drop('NOTA', axis=1)  
+y = numeric_columns['NOTA']
+
+regression_model = LinearRegression()
+regression_model.fit(X, y)
+
+joblib.dump(regression_model, 'models/regresion_lineal.pkl')
